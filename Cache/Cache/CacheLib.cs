8085970f1aeb14aa5931f84f0b2a0dfeb0c6bd08
Vlad <Novakovsky>
2020-01-19 18:40:12 +0200

    + NuGet package log4net
    + CacheLib
    +CacheLib.Initialize {log4net.Config.XmlConfigurator.Configure();}
    +LogError
    +LogDebug
    +LogInfo

---
+++ Cache/Cache/CacheLib.cs
@@ -0,0 +1,31 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cache
+{
+    public static class CacheLib
+    {
+        private static readonly ILog _log = LogManager.GetLogger("LOGGER");
+        public static void Initialize()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+        }
+        public static void LogError(Exception ex)
+        {
+            _log.Error(ex.Message);
+            _log.Error(ex.StackTrace);
+        }
+        public static void LogDebug(string message)
+        {
+            _log.Debug(message);
+        }
+        public static void LogInfo(string message)
+        {
+            _log.Info(message);
+        }
+    }
+}
