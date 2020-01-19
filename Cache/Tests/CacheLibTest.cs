f7d2c3b7473e71e4253cc000bdc62d066b964a86
Vlad <Novakovsky>
2020-01-19 19:18:00 +0200

    + Unit Test project
    log works in test

---
+++ Cache/Tests/CacheLibTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Cache;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class CacheLibTest
+    {
+        [TestMethod]
+        public void LogDebug()
+        {
+            CacheLib.Initialize();
+            CacheLib.LogDebug("debug test string");
+        }
+    }
+}
