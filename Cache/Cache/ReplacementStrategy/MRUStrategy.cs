57da185a2f82b95c8fde83843d91e45c8d755d34
Vlad <Novakovsky>
2020-02-13 19:19:24 +0200

    [~] fix MRUStrategy Less to Greater
    All Tests OK

--- Cache/Cache/ReplacementStrategy/MRUStrategy.cs
+++ Cache/Cache/ReplacementStrategy/MRUStrategy.cs
@@ -5,7 +5,7 @@ namespace Cache.ReplacementStrategy
     public class MRUStrategy<Tag> : RecentlyUsedStrategy<Tag> where Tag : unmanaged
     {
         public MRUStrategy(CacheGeometry cacheGeometry)
-            :base(cacheGeometry, Functor.Less<long>())
+            :base(cacheGeometry, Functor.Greater<long>())
         {
         }
     }
